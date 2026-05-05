51904ffc0762c4a9a871b591359874259c0db2ee
Eneko123 <eneko_iglesias@euneiz.com>
2026-05-05 17:35:25 +0200

    Clase

--- InfoGrafica/PointLight.cpp
+++ InfoGrafica/PointLight.cpp
@@ -2,26 +2,28 @@
 
 PointLight::PointLight() : Light()
 {
-    pos = glm::vec3(0.0f, 0.0f, 0.0f);
-    constantValue = 1.0f;
-    linealValue = 0.0f;
-	exponentialValue = 0.0f;
+	pos = glm::vec3(0);
+	constantValue = 0;
+	linearValue = 0;
+	exponentialValue = 0;
 }
 
 PointLight::PointLight(glm::vec3 color, GLfloat ambientIntensity, GLfloat diffuseIntensity, glm::vec3 pos, float constant, float linear, float exponential)
-    : Light(color, ambientIntensity, pos, diffuseIntensity)
+	: Light(color, ambientIntensity, diffuseIntensity)
 {
-    this->pos = pos;
-    this->constantValue = constant;
-    this->linealValue = linear;
-    this->exponentialValue = exponential;
+	this->pos = pos;
+	constantValue = constant;
+	linearValue = linear;
+	exponentialValue = exponential;
 }
 
 void PointLight::UseLight(UniformPointLight uniformPointLight)
 {
-    Light::UseLight(uniformPointLight);
-	glUniform3f(uniformPointLight.uniformPosition, pos.x, pos.y, pos.z);
-	glUniform1f(uniformPointLight.uniformConstant, constantValue);
-	glUniform1f(uniformPointLight.uniformLinear, linealValue);
+	Light::UseLight(uniformPointLight.base);
+	glUniform3f(uniformPointLight.uniformPos, pos.x, pos.y, pos.z);
 	glUniform1f(uniformPointLight.uniformExponential, exponentialValue);
+	glUniform1f(uniformPointLight.uniformLinear, linearValue);
+	glUniform1f(uniformPointLight.uniformConstant, constantValue);
+
+
 }
